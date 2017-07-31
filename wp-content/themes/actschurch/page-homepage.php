ff0cdc73fb46a355fc45e3aceb85bdb32e7bb8be
screenastronaut <screenastronaut@gmail.com>
2017-07-31 15:54:11 +0800

    Homepage: Hi There and Real Stories

--- wp-content/themes/actschurch/page-homepage.php
+++ wp-content/themes/actschurch/page-homepage.php
@@ -7,37 +7,38 @@
  * @package actschurch
  */
 
+// TODO: get play button image
 get_header(); ?>
 
 <div class="splash-image">
 	<h1>A Place For all</h1>
 	<a href="#time-and-locations" class="button green">Time & Locations</a>
 	<a href="what-to-expect" class="button blue">What to Expect</a>
+	<i class="main-video fa fa-5x fa-play-circle-o" aria-hidden="true"></i>
 </div>
 
 
-<div id="content" class="site-content container">
+<div id="content" class="site-content">
 
-	<div id="primary" class="content-area col-lg-12 col-md-12 col-sm-12 col-xs-12">
+	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-			<?php
-			if ( have_posts() ) :
+			<section class="introduction container">
+				<div class="col-lg-push-2 col-md-push-2 col-lg-8 col-md-8 col-sm-12 col-xs-12">
+					<?php echo get_field('introduction_text'); ?>
+				</div>
+			</section>
 
-				/* Start the Loop */
-			while ( have_posts() ) : the_post();
-			get_template_part( 'template-parts/post/content', get_post_format() );
+			<section class="grid container-fluid">
+				<div class="col-lg-6">test</div>
+				<div class="col-lg-6">test</div>
+			</section>
 
-			endwhile;
-
-			the_posts_pagination();
-
-			else :
-
-				get_template_part( 'template-parts/post/content', 'none' );
-
-			endif;
-			?>
+			<section class="stories">
+				<div class="col-lg-push-2 col-md-push-2 col-lg-8 col-md-8 col-sm-12 col-xs-12">
+					<?php echo get_field('real_stories_text'); ?>
+				</div>
+			</section>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
