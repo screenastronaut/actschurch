b6afa6b89f0c7d4de3e78efd646bff3cc623384e
screenastronaut <screenastronaut@gmail.com>
2017-08-20 21:10:28 +0800

    Updated WP Core

--- wp-content/themes/actschurch/functions.php
+++ wp-content/themes/actschurch/functions.php
@@ -183,3 +183,60 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+// Register Custom Post Type
+function leaders_cpt() {
+
+	$labels = array(
+		'name'                  => _x( 'Leaders', 'Post Type General Name', 'text_domain' ),
+		'singular_name'         => _x( 'Leader', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'             => __( 'Leaders', 'text_domain' ),
+		'name_admin_bar'        => __( 'Leader', 'text_domain' ),
+		'archives'              => __( 'Item Archives', 'text_domain' ),
+		'attributes'            => __( 'Item Attributes', 'text_domain' ),
+		'parent_item_colon'     => __( 'Parent Leader:', 'text_domain' ),
+		'all_items'             => __( 'All Leaders', 'text_domain' ),
+		'add_new_item'          => __( 'Add New Leader', 'text_domain' ),
+		'add_new'               => __( 'Add New', 'text_domain' ),
+		'new_item'              => __( 'New Leader', 'text_domain' ),
+		'edit_item'             => __( 'Edit Leader', 'text_domain' ),
+		'update_item'           => __( 'Update Leader', 'text_domain' ),
+		'view_item'             => __( 'View Leader', 'text_domain' ),
+		'view_items'            => __( 'View Leaders', 'text_domain' ),
+		'search_items'          => __( 'Search Leader', 'text_domain' ),
+		'not_found'             => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+		'featured_image'        => __( 'Featured Image', 'text_domain' ),
+		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+		'insert_into_item'      => __( 'Insert into Leader', 'text_domain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this leader', 'text_domain' ),
+		'items_list'            => __( 'Leaders list', 'text_domain' ),
+		'items_list_navigation' => __( 'Leaders list navigation', 'text_domain' ),
+		'filter_items_list'     => __( 'Filter leaders list', 'text_domain' ),
+	);
+	$args = array(
+		'label'                 => __( 'Leader', 'text_domain' ),
+		'description'           => __( 'Custom Post Type for Leaders', 'text_domain' ),
+		'labels'                => $labels,
+		'supports'              => array(''),
+		'taxonomies'            => array(''),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-businessman',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,		
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'leaders', $args );
+
+}
+add_action( 'init', 'leaders_cpt', 0 );
