c5cd82e4699f3ccbafe5e4498dfab2ba88d37ad7
screenastronaut <screenastronaut@gmail.com>
2017-09-30 01:32:44 +0800

    Entire box for featured stories is linkable

--- wp-content/themes/actschurch/template-parts/post/content-featured-stories.php
+++ wp-content/themes/actschurch/template-parts/post/content-featured-stories.php
@@ -12,11 +12,13 @@ $story_pic = get_field('photo');
 $story_url = get_permalink();
 ?>
 
-<div class="real-story col-lg-3 col-md-3 col-sm-12 col-xs-12">
-	<div class="text text-<?=$class?>">
-		<div class="story-title">
-			<h4><a href="<?=$story_url?>"><?=$story_title?></a></h4>
+<a href="<?=$story_url?>" class="real-story-link">
+	<div class="real-story col-lg-3 col-md-3 col-sm-12 col-xs-12">
+		<div class="text text-<?=$class?>">
+			<div class="story-title">
+				<h4><?=$story_title?></h4>
+			</div>
 		</div>
+		<div class="pic" style="background:url(<?=$story_pic?>);background-size:cover;background-repeat:no-repeat;"></div>
 	</div>
-	<div class="pic" style="background:url(<?=$story_pic?>);background-size:cover;background-repeat:no-repeat;"></div>
-</div>
+</a>
