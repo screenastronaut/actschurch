e783c8e5c50371c76a0d9877749976a63cb63ef6
Stefanie <stefanie.chong@text100.com.my>
2017-10-05 16:46:20 +0800

    Sticky header and orange logo

---
+++ wp-content/themes/actschurch/template-parts/header/site-branding-orange.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Displays header site branding
+ *
+ * @package actschurch
+ */
+
+?>
+<div class="site-branding">
+	<a href="<?php echo esc_url( home_url( '/' ) );?>"><img src="<?php echo get_template_directory_uri(); ?>/images/Acts-logo-orange.png" alt="Acts Church Logo"></a>
+</div><!-- .site-branding -->
--- wp-content/themes/actschurch/template-parts/header/site-branding.php
+++ wp-content/themes/actschurch/template-parts/header/site-branding.php
@@ -4,6 +4,7 @@
  *
  * @package actschurch
  */
+
 ?>
 <div class="site-branding">
 	<a href="<?php echo esc_url( home_url( '/' ) );?>"><img src="<?php echo get_template_directory_uri(); ?>/images/Acts-logo.png" alt="Acts Church Logo"></a>
