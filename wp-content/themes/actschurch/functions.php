11e33922b9ed5aea0a0dd9ef80a14fb8e76afc81
screenastronaut <screenastronaut@gmail.com>
2017-10-23 23:37:33 +0800

    Announcement CPT

--- wp-content/themes/actschurch/functions.php
+++ wp-content/themes/actschurch/functions.php
@@ -375,6 +375,63 @@ function videos_cpt() {
 }
 add_action( 'init', 'videos_cpt', 0 );
 
+// Register Leaders Custom Post Type
+function announcements_cpt() {
+
+	$labels = array(
+		'name'                  => _x( 'Announcements', 'Post Type General Name', 'text_domain' ),
+		'singular_name'         => _x( 'Announcement', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'             => __( 'Announcements', 'text_domain' ),
+		'name_admin_bar'        => __( 'Announcement', 'text_domain' ),
+		'archives'              => __( 'Item Archives', 'text_domain' ),
+		'attributes'            => __( 'Item Attributes', 'text_domain' ),
+		'parent_item_colon'     => __( 'Parent Announcement:', 'text_domain' ),
+		'all_items'             => __( 'All Announcements', 'text_domain' ),
+		'add_new_item'          => __( 'Add New Announcement', 'text_domain' ),
+		'add_new'               => __( 'Add New', 'text_domain' ),
+		'new_item'              => __( 'New Announcement', 'text_domain' ),
+		'edit_item'             => __( 'Edit Announcement', 'text_domain' ),
+		'update_item'           => __( 'Update Announcement', 'text_domain' ),
+		'view_item'             => __( 'View Announcement', 'text_domain' ),
+		'view_items'            => __( 'View Announcements', 'text_domain' ),
+		'search_items'          => __( 'Search Announcement', 'text_domain' ),
+		'not_found'             => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+		'featured_image'        => __( 'Featured Image', 'text_domain' ),
+		'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+		'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+		'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+		'insert_into_item'      => __( 'Insert into Announcement', 'text_domain' ),
+		'uploaded_to_this_item' => __( 'Uploaded to this Announcement', 'text_domain' ),
+		'items_list'            => __( 'Announcements list', 'text_domain' ),
+		'items_list_navigation' => __( 'Announcements list navigation', 'text_domain' ),
+		'filter_items_list'     => __( 'Filter Announcements list', 'text_domain' ),
+	);
+	$args = array(
+		'label'                 => __( 'Announcement', 'text_domain' ),
+		'description'           => __( 'Custom Post Type for Announcements', 'text_domain' ),
+		'labels'                => $labels,
+		'supports'              => array('title'),
+		'taxonomies'            => array(''),
+		'hierarchical'          => false,
+		'public'                => true,
+		'show_ui'               => true,
+		'show_in_menu'          => true,
+		'menu_position'         => 5,
+		'menu_icon'             => 'dashicons-flag',
+		'show_in_admin_bar'     => true,
+		'show_in_nav_menus'     => true,
+		'can_export'            => true,
+		'has_archive'           => true,
+		'exclude_from_search'   => false,
+		'publicly_queryable'    => true,
+		'capability_type'       => 'page',
+	);
+	register_post_type( 'announcements', $args );
+
+}
+add_action( 'init', 'announcements_cpt', 0 );
+
 function my_acf_google_map_api( $api ){
 
 	$api['key'] = 'AIzaSyB7pYySi9L0peixxr-Le2__2f2TCo5Kdag';
