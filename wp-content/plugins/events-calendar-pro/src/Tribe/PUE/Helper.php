83ed6dded5342b7ab6fcc7825d2886e6e6cfde68
Stefanie <stefanie.chong@text100.com.my>
2017-11-03 15:33:23 +0800

    Events Calendar Pro

---
+++ wp-content/plugins/events-calendar-pro/src/Tribe/PUE/Helper.php
@@ -0,0 +1,10 @@
+<?php
+
+class Tribe__Events__Pro__PUE__Helper {
+
+	/**
+	 * @var string Helper data
+	 */
+	const DATA = 'e2e76be2eaa4e172f6b5389abaf94038c333f743';
+
+}
---
+++ wp-content/plugins/the-events-calendar-filterbar/src/Tribe/PUE/Helper.php
@@ -0,0 +1,10 @@
+<?php
+
+class Tribe__Events__Filterbar__PUE__Helper {
+
+	/**
+	 * @var string Helper data
+	 */
+	const DATA = '5bef993e5b825d72b79c0b77e11df4fddf3f314e';
+
+}
